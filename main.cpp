f78deb5412c7bdc50d359c3ad9ca6e06443095a8
Nikodemos Koutsoheras <git@koutsoheras.com>
2022-10-03 15:30:02 -0400

    update argument string for output file

--- main.cpp
+++ main.cpp
@@ -330,7 +330,7 @@ void parseCommandLine(int argc, char** const argv)
   int ret;
   optind = 1;
 
-  while ((ret = getopt(argc, argv, "f:r:n:lhp:m:x:bg:t:ws:z:ud:o:")) != -1) {
+  while ((ret = getopt(argc, argv, "f:r:n:lhp:m:x:bg:t:ws:z:ud:o:a:")) != -1) {
     switch (ret) {
     case 'f':
       inputFileName.assign(optarg);
